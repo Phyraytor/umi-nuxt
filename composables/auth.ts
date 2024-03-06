bf405ce1f5785694efaf78790da073a6f2c9cc47
s.sivkov <s.sivkov@jetmail.cc>
2024-03-06 18:17:35 +0300

    fix auth

--- composables/auth.ts
+++ composables/auth.ts
@@ -1,5 +1,11 @@
 export function useAuth() {
-  const { $fetchData } = useNuxtApp()
+  const {$fetchData} = useNuxtApp()
+
+  const authWithoutBackend = () => {
+    // Todo: remove this before connect API
+    useCookie('auth:token').value = 'TRUE'
+    navigateTo('/')
+  }
 
   const logout = () => {
     useCookie('auth:token').value = undefined
@@ -9,24 +15,29 @@ export function useAuth() {
     login: string | null
     password: string | null
   }) => {
-    // Todo move to onResponse
-    useCookie('auth:token').value = 'TRUE'
-    navigateTo('/')
-      return await $fetchData({
+    return await $fetchData({
       api: '/auth/sign-in',
       body: payload,
       method: 'post',
       args: {
-        async onResponse({ response }: any) {
-          if (!response.ok) return
-
+        async onRequestError({ }: any) {
+          authWithoutBackend();
+        },
+        async onResponse({response}: any) {
+          if (!response.ok) {
+            authWithoutBackend();
+            return
+          }
+          const { _data } = response
+          useCookie('auth:token').value = _data?.access_token
+          navigateTo('/')
         },
       },
     })
   }
 
-  const isLoggedIn = () =>  {
-    const refresh = useCookie('auth:token', { watch: true })
+  const isLoggedIn = () => {
+    const refresh = useCookie('auth:token', {watch: true})
     return refresh.value !== undefined
   }
   return {
